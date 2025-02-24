1a4282dc1f0a9d1f4a149a206126edadb07eaf5b
wangWking <842749351@qq.com>
2025-02-24 17:54:09 +0800

    更新X1 urdf J6初始姿态，优化滑块控制

--- turn_on_mercury_robot/scripts/slider_control.py
+++ turn_on_mercury_robot/scripts/slider_control.py
@@ -5,8 +5,8 @@ This file obtains the joint angle of the manipulator in ROS,
 and then sends it directly to the real manipulator using `pymycobot` API.
 This file is [slider_control.launch] related script.
 Passable parameters:
-      port1: Left arm serial port string. Default is "/dev/ttyTHS1"
-      port2: Right arm serial port string. Default is "/dev/ttyS0"
+      port1: Left arm serial port string. Default is "/dev/left_arm"
+      port2: Right arm serial port string. Default is "/dev/right_arm"
       Baud rate: Left and right arm serial port baud rate. The default value is 115200.
 """
 import math
@@ -35,34 +35,42 @@ def callback(data):
     left_arm = data_list[:7]
     right_arm = data_list[7:-3]
     middle_arm = data_list[-3:]
-    
+    # 为左臂和右臂的 J6 关节角度加上偏移量 90°
+    left_arm[5] = left_arm[5] + 90
+    right_arm[5] = right_arm[5] + 90
     print('left_angles: {}'.format(left_arm))
     print('right_angles: {}'.format(right_arm))
     print('middle_angles: {}'.format(middle_arm))
     
-    l.send_angles(left_arm, 25)
-    r.send_angles(right_arm, 25)
-    r.send_angle(11, middle_arm[0], 25)
-    r.send_angle(12, middle_arm[1], 25)
-    r.send_angle(13, middle_arm[2], 25)
+    l.send_angles(left_arm, 16, _async=True)
+    r.send_angles(right_arm, 16, _async=True)
+    r.send_angle(11, middle_arm[0], 16, _async=True)
+    r.send_angle(12, middle_arm[1], 16, _async=True)
+    r.send_angle(13, middle_arm[2], 16, _async=True)
 
 
 def listener():
     global l, r
     rospy.init_node("control_slider", anonymous=True)
-
-    rospy.Subscriber("joint_states", JointState, callback)
-    port1 = rospy.get_param("~port1", "/dev/ttyTHS0")
-    port2 = rospy.get_param("~port2", "/dev/ttyACM0")
+    
+    port1 = rospy.get_param("~port1", "/dev/left_arm")
+    port2 = rospy.get_param("~port2", "/dev/right_arm")
     baud = rospy.get_param("~baud", 115200)
     print('left arm: {}, {}'.format(port1, baud))
     print('right arm: {}, {}'.format(port2, baud))
     l = Mercury(port1, baud)
     r = Mercury(port2, baud)
     time.sleep(0.05)
-    # l.set_fresh_mode(1)
-    # r.set_fresh_mode(1)
-    # time.sleep(0.05)
+    l.set_movement_type(2)
+    r.set_movement_type(2)
+    time.sleep(0.05)
+    l.set_vr_mode(1)
+    r.set_vr_mode(1)
+    time.sleep(0.05)
+    l.set_filter_len(3, 20)
+    r.set_filter_len(3, 20)
+    time.sleep(0.05)
+    rospy.Subscriber("joint_states", JointState, callback)
     # spin() simply keeps python from exiting until this node is stopped
     # spin()只是阻止python退出，直到该节点停止
     print("spin ...")
